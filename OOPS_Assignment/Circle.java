c80bebb509de622582c662c8342e13909ba5eea4
--add <shreyanshmishra1202@gmail.com>
2025-09-03 17:03:39 +0530

    Shreyansh Adding. OOPS Assignment Circle ques

---
+++ OOPS_Assignment/Circle.java
@@ -0,0 +1,15 @@
+//Write a Circle class with a radius attribute. Use constructor chaining to initialize radius with default and user-provided values.
+
+package OOPS_Assignment;
+
+public class Circle {
+    int radius;
+
+    public Circle(int radius){
+        this.radius = radius;
+    }
+
+    public Circle(){
+
+    }
+}
