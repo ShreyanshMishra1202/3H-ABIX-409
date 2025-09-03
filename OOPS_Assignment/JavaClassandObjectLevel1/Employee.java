84fdf765163b1fadd825266eb2036372cebd272f
--add <shreyanshmishra1202@gmail.com>
2025-09-03 17:13:54 +0530

    Shreyansh Add. OOPS Java Class Level 1

---
+++ OOPS_Assignment/JavaClassandObjectLevel1/Employee.java
@@ -0,0 +1,15 @@
+package OOPS_Assignment.JavaClassandObjectLevel1;
+
+public class Employee {
+    String name;
+    int id;
+    int salary;
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        System.out.println(emp.name);
+        System.out.println(emp.id);
+        System.out.println(emp.salary);
+    }
+
+}
